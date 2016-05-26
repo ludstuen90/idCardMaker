8cb2430a48461af6e51c3544c0e488c5af44ac40
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-05-25 22:36:42 -0500

    First commit. Skeleton of the idea included.

---
+++ scripts/interaction.js
@@ -0,0 +1,27 @@
+console.log("Hello");
+
+
+var makeId= function(){
+  var firstName = document.getElementById('fname').value;
+  var lastName = document.getElementById('lname').value;
+  var nickName = document.getElementById('nname').value;
+  var heightt = document.getElementById('height').value;
+  var weightt = document.getElementById('weight').value;
+  var address = document.getElementById('addy').value;
+  var cityy = document.getElementById('city').value;
+  var statee = document.getElementById('state').value;
+
+
+  console.log("First name is: " + firstName);
+  console.log("Last name is: " + lastName);
+  console.log("Nickname is: " + nickName);
+  console.log("Height is: " + heightt);
+  console.log("Weight is: " + weightt);
+  console.log("Address is: " + address);
+  console.log("City is: " + cityy);
+  console.log("State is: " + statee);
+
+
+
+
+};
